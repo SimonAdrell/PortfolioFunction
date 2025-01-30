3f1db92af68c08d33d216cf3fd984536a958cfab
Simon Adrell <simon.adrell@gmail.com>
2025-01-30 16:25:10 +0100

    Sending get request to website

--- TimerTriggerPing.cs
+++ TimerTriggerPing.cs
@@ -14,10 +14,15 @@ namespace CoBySi.Function
         }
 
         [Function("TimerTriggerPing")]
-        public void Run([TimerTrigger("0 */3 * * * *")] TimerInfo myTimer)
+        public async Task Run([TimerTrigger("0 */3 * * * *")] TimerInfo myTimer)
         {
             _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync("https://www.codebysimon.com");
+                _logger.LogError("HTTP request status code: {StatusCode}", response.StatusCode);
+            }
+
             if (myTimer.ScheduleStatus is not null)
             {
                 _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
